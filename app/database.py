b94e79a475031caf2e92c5480b10877f4a88378b
Brandon <brandonbgrayson@gmail.com>
2022-08-26 14:14:12 -0400

    database model for Services inquiry defined

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/pulse_media"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:5432/pulse_media"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
-from sqlalchemy import Column
+from sqlite3 import Timestamp
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from .database import Base
+from sqlalchemy.sql.expression import text
+
+class Services(Base):
+    __tablename__ = "services"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    firstName = Column(String, nullable=False)
+    lastName = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
