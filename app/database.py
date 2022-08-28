9a4fb428945e9ece79539d375e9d7099b8efb6bd
Brandon <brandonbgrayson@gmail.com>
2022-08-28 13:36:32 -0400

    able to connect to db and services table built

--- app/database.py
+++ app/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:5432/pulse_media"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/pulse_media"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from email.policy import default
 import json
-from fastapi import FastAPI, Header, Response, status
+from fastapi import FastAPI, Header, Response, status, Depends
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
+from . import models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 origins = [
     'http://localhost:3000'
@@ -28,7 +42,7 @@ class Service(BaseModel):
     services: List[str]
 
 @app.post("/service", status_code=status.HTTP_201_CREATED)
-async def add_service(service: Service):
+async def add_service(service: Service, db: Session = Depends(get_db)):
     try:
         print((service))
     except ValueError as error:
